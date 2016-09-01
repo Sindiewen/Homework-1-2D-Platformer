ecd128139724c7b8995648cbc4a303b84e55efe0
Rachel <sindiewen@gmail.com>
2016-09-01 14:37:26 -0700

    Finished Project - Ready for submission

--- Homework 1 - 2D Platformer/Assets/_Scripts/PiranhaPlantController.cs
+++ Homework 1 - 2D Platformer/Assets/_Scripts/PiranhaPlantController.cs
@@ -21,48 +21,42 @@ public class PiranhaPlantController : MonoBehaviour
 	// Public Variables
 	public GameObject player;		// Stores reference to the player GameObject
 
+
+
 	public float pauseTime = 1.0f;	// Sets how long the plant should pause before doing anything else
 	public float riseHeight;		// Sets how high the plant will rise
 
 	// Private Variables
 
-	private bool CanRise;
-
 	private float origX;			// Stores the original position of the plant X
 	private float origY;			// Stores the original position of the plant Y
 
 	private CircleCollider2D circleCol;				// Stores refrerence to the CircleCollider2D
-	private CircleCollider2D circleRiseBoundary;
 
 	// Starts at the first frame of the game
 	void Start()
 	{
 		circleCol = gameObject.GetComponent<CircleCollider2D>();	// Gets the circle collider2D Component
-		circleRiseBoundary = gameObject.GetComponent<CircleCollider2D();
 
 		origX = transform.position.x;	// Stores the original position of the plant X
 		origY = transform.position.y;	// Stores the original position of the plant Y
 	}
 
+
+
 	// When the player enters the trigger
 	void OnTriggerEnter2D(Collider2D col)
 	{
 
-		// The Plant can rise out of the pipe
-		CanRise = true;
+		// Disables the circle collider 2D component
+		circleCol.radius = 0.2f;
+	
+		// Changes the offset of the collider box
+		circleCol.offset = new Vector2(0, -20);
 
-		// If the plant can rise out of the pipe
-		if (CanRise)
-		{
-			// Disables the circle collider 2D component
-			circleCol.radius = 0.2f;
+		// Starts coroutine to shoot player
+		StartCoroutine(ShootPlayer());
 
-			// Changes the offset of the collider box
-			circleCol.offset = new Vector2(0, -20);
-
-			// Starts coroutine to shoot player
-			StartCoroutine(ShootPlayer());
-		}
 	}
 
 
@@ -74,7 +68,7 @@ public class PiranhaPlantController : MonoBehaviour
 		float xPos = transform.position.x;	// Stores x corordinate of Plant's original position
 
 		// Wait a certain ammount of time before the next statement gets called
-		yield return new WaitForSeconds(pauseTime/2);
+		yield return new WaitForSeconds(pauseTime * 0.5f);
 
 		//Moves plant upwards
 		this.transform.position = new Vector2 (xPos, yPos + riseHeight);
